952e4cc40c7a8fb4c2f7c45dc99377c18808136f
fwefwal <bogdankyzmin11@gmail.com>
2025-03-30 23:51:32 +0300

    Update queries.ts

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,19 +1,76 @@
-import type { Database } from "sqlite3"
+import type { Insertable } from "kysely"
+import type getConnection from "./connection"
+import type { DB } from "kysely-codegen"
 
-function logFirstFourtyAnswers(db: Database) {
-  db.each("select * from answer limit 40", (error, row) => {
-    if (!error) {
-      console.log(row)
-    }
-  })
+type Connection = ReturnType<typeof getConnection>
+
+async function logAllQuestions(connection: Connection) {
+  try {
+    const result = await connection.selectFrom("Question").selectAll().execute()
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-function logTopFiveCountries(db: Database) {
-  db.each("select country, count(*) as count from Answer limit 5 order by count desc", (error, row) => {
-    if (!error) {
-      console.log(row)
-    }
-  })
+async function createSurvey(
+  connection: Connection,
+  newSurvey: Insertable<DB["Survey"]>
+) {
+  try {
+    await connection
+      .insertInto("Survey")
+      .values(newSurvey)
+      .returningAll()
+      .executeTakeFirstOrThrow()
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-export { logFirstFourtyAnswers, logTopFiveCountries }
+async function getTop5Countries(connection: Connection) {
+  try {
+    const countryCounts = await connection.selectFrom("Answer").where("QuestionID", "=", 3)
+      .select(["AnswerText"])
+      .groupBy("AnswerText")
+      .select([
+        "AnswerText",
+        connection.fn.countAll().as("count")
+      ])
+      .orderBy("count", "desc")
+      .execute()
+
+    const top5Countries = countryCounts.slice(0, 5)
+    console.log("Топ-5 стран", top5Countries)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function getMentalHealthStats(connection: Connection) {
+  try {
+    const totalRespondents = await connection.selectFrom("Answer").where("QuestionID", "=", 33)
+    .select([
+      "AnswerText",
+      connection.fn.countAll().as("count")
+    ])
+      .executeTakeFirst()
+
+    const yesCount = await connection.selectFrom("Answer").where("QuestionID", "=", 33).where("AnswerText", "=", "Yes")
+    .select([
+      "AnswerText",
+      connection.fn.countAll().as("count")
+    ])
+      .executeTakeFirst()
+
+    const total = totalRespondents?.count || 1
+    const yes = yesCount?.count || 0
+    const percentage = ((yes / total) * 100).toFixed(2)
+    
+    console.log(`С ментальными расстройствами: ${yes} (${percentage}%)`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { logAllQuestions, createSurvey, getTop5Countries, getMentalHealthStats }
